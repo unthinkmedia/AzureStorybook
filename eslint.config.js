d8d1b4a4a89be1695715c0edfb1ad84a06e0e72f
Alex Britez <alexbritez@microsoft.com>
2026-03-09 19:28:31 -0400

    fix: include eslint.config.js in repo

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import eslint from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import reactPlugin from 'eslint-plugin-react';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  eslint.configs.recommended,
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      react: reactPlugin,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+  },
+  prettierConfig,
+];
